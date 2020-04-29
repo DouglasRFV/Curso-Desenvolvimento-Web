47f7b9bc79d375e92caab6e60f82b7d85e6a320a
DouglasRfV <douglas.rfvianna@gmail.com>
2020-04-29 19:14:20 -0300

    first commit

---
+++ JavaScript/meu_script.js
@@ -0,0 +1,6 @@
+alert('Olá, seja bem vindo ao curso!!!')
+/*
+	O código abaixo serve para selecionar um elemento no DOM
+	e atualizar o valor desse elemento com uma string.
+*/
+document.getElementById('nome').value = 'Oi'
