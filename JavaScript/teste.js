54aad79f22ff95df0f89466ddea1d6f0a455c923
DouglasRfV <douglas.rfvianna@gmail.com>
2020-04-30 00:43:15 -0300

    aulas 29abr2020

---
+++ JavaScript/teste.js
@@ -0,0 +1,10 @@
+
+const a = 1;
+const b = '1';
+
+// == & != TESTA VALOR DA VARIÁVEL
+// === & !== TESTA VALOR E TIPO DA VARIÁVEL
+console.log(a == b);
+console.log(a === b);
+console.log(a != b);
+console.log(a !== b);
